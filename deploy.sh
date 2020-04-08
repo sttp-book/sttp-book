7a6b4e0ac5f214d862feca61bcf09ddf23bc2f92
Frank Mulder <F.Mulder@tudelft.nl>
2020-04-08 19:46:55 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,18 @@
 gitbook build
-rm -rf ../sttp-book.github.io/assets/ ../sttp-book.github.io/chapters/ ../sttp-book.github.io/gitbook/ ../sttp-book.github.io/gitbook_to_pdf.sh ../sttp-book.github.io/index.html ../sttp-book.github.io/search_index.json
-cp -r _book/* ../sttp-book.github.io/
 cd ../sttp-book.github.io
+
+git pull origin master
+
+rm -rf assets/ chapters/ gitbook/ gitbook_to_pdf.sh index.html search_index.json
+cp -r ../sttp-book/_book/* .
+
 rm -rf drawio 
 rm -rf answers 
 rm -rf includes 
 rm -rf latex-conf
 rm *.sh
-git pull
+
 git add -A
 git commit -am "deploy"
 git push origin master
 cd ../sttp-book
-
